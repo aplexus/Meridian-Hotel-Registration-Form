72b6dfcefb9d38ecd5379907f879b92d4fcd808d
Aleksey Popov <aplexus11@yandex.ru>
2019-09-28 10:32:59 +0300

    Initial Commit

---
+++ Meridian Hotel Registration Form/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meridian Hotel Registration Form
+//
+//  Created by Aleksey Popov on 28/09/2019.
+//  Copyright © 2019 Aleksey Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
